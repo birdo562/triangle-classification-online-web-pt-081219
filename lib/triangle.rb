bd52d0950866bc92f4a90fc8d94cb12ce9b75277
Roberto Medina <robby.medina562@gmail.com>
2019-09-30 17:23:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,16 @@ def initialize(a, b, c)
 end
 def triangle_check
   legal_triangle = [(a+b>c), (a+c>b), (b+c>a)]
-  [a, b, c].each {|side| legal_triangle << false if side <= 0
+  [a, b, c].each {|side| legal_triangle << false if side <= 0}
+end
 def kind
   triangle_check
+  if a == b && b == c
+    :equilateral
+    elsif
+    a == b || b == c || a == c 
+    :isosceles
+  else
+    :scalene
+  end
+end
