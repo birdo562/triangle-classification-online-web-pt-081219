a0ab3e04e0f82cab1f0da3cf2dbe634fbd6e1524
Roberto Medina <robby.medina562@gmail.com>
2019-09-30 16:52:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,5 +11,6 @@ class Triangle
   
   def triangle_check
     triangle= [(side1+side2>side3), (side1+side3>side2), (side2+side3>side1)]
-    [side1,side2,side3].each {|side| triangle << false
+    [side1,side2,side3].each {|side| triangle << false if side = 0}
+    
 end
