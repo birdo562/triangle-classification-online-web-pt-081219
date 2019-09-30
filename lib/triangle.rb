fc0943b5b5f3518232c86f7e7c04557545519382
Roberto Medina <robby.medina562@gmail.com>
2019-09-30 17:25:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ end
 def triangle_check
   legal_triangle = [(a+b>c), (a+c>b), (b+c>a)]
   [a, b, c].each {|side| legal_triangle << false if side <= 0}
-  raise TriangleError if 
+  raise TriangleError if legal_triangle.include?(false)
+end
+class TriangleError < StandardError
 end
 def kind
   triangle_check
